c72044cfe830088ecae2dc6129e948c687e6a7b6
Saulo Oliveira <saulo.accacio@gmail.com>
2025-09-16 15:03:32 -0300

    Criating migrations before apply

--- main/models.py
+++ main/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -16,6 +20,14 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
 
     def __str__(self):
         return self.title
